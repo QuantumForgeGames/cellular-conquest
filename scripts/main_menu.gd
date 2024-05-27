26a220ce7265561831a5dd5559903226e5ddf966
JoshEthan <joshuaeris@gmail.com>
2024-05-27 15:24:17 -0600

    Basic menu functionality.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_settings_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
