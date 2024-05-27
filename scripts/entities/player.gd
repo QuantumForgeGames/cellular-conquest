f928ca7344c9595ff5a79161beb19dae6575dbc2
Akshay <sakshays.2000@gmail.com>
2024-05-27 23:07:51 +0200

    add health and size dependance

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -4,6 +4,18 @@ extends CharacterBody2D
 @export var MAX_SPEED := 400.
 @export var MAX_MOUSE_DIST := 100.
 
+# Health/Size 
+@export var BASE_HEALTH := 1
+@export var BASE_SIZE: float = scale.x 
+@export var SIZE_SCALE_FACTOR := 0.01 # factor relating size and health
+@export var SIZE_TWEEN_RATE := 0.01 # REVISIT! linear rate might not be feasible for larger sizes
+@export var health: int = BASE_HEALTH :
+	set (value):
+		update_size(value, health)
+		health = value
+
+var size_tween: Tween = null
+
 # Dash ability
 @export var DASH_SPEED := 1000.
 @export var DASH_DURATION := 0.3
@@ -36,7 +48,13 @@ func _input(_event: InputEvent) -> void:
 		projectile.global_position = global_position
 		projectile.velocity = PROJECTILE_SPEED * (get_global_mouse_position() - global_position).normalized()
 		add_sibling(projectile)
-		
+
 func toggle_dash():
 	is_dashing = not is_dashing
 
+func update_size(old_health: int, current_health: int):
+	var target_scale := (SIZE_SCALE_FACTOR * (old_health - BASE_HEALTH) + BASE_SIZE) * Vector2.ONE
+	
+	if size_tween: size_tween.kill()
+	size_tween = get_tree().create_tween()
+	size_tween.tween_property(self, "scale", target_scale, SIZE_TWEEN_RATE * abs(current_health - old_health))
