8954fa7572488c856fe5c35e2652ef873d36d447
Akshay <sakshays.2000@gmail.com>
2024-05-28 00:49:51 +0200

    add absorption mechanic

--- scripts/entities/hitbox.gd
+++ scripts/entities/hitbox.gd
@@ -1,11 +1,15 @@
 extends Area2D
+class_name Hitbox
+
+signal damage_recieved
 
 @onready var entity := get_parent()
 @export var BASE_HEALTH := 1
 @export var BASE_SIZE: float = scale.x 
 @export var SIZE_SCALE_FACTOR := 0.01 # factor relating size and health
 @export var SIZE_TWEEN_RATE := 0.01 # REVISIT! linear rate might not be feasible for larger sizes
-@export var health: int = BASE_HEALTH :
+
+var health: int = BASE_HEALTH :
 	set (value):
 		update_size(health, value)
 		health = value
@@ -17,3 +21,13 @@ func update_size(old_health: int, current_health: int):
 	if size_tween: size_tween.kill()
 	size_tween = get_tree().create_tween()
 	size_tween.tween_property(entity, "scale", target_scale, SIZE_TWEEN_RATE * abs(current_health - old_health))
+
+func on_damage_recieved(val: int):
+	health -= val
+	damage_recieved.emit()
+	
+func _on_area_entered(area: Area2D) -> void:
+	if entity is Player and area is Hitbox:
+		var loser = self if (entity.scale.x <= area.entity.scale.x) else area
+		health += loser.health
+		loser.entity.on_absorbed()
--- scripts/entities/projectile.gd
+++ scripts/entities/projectile.gd
@@ -1,6 +1,17 @@
 extends Area2D
+class_name Projectile
 
+@export var damage: int = 1
 var velocity := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta	
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Hitbox:
+		area.on_damage_recieved(damage)
+		_on_hit()
+		
+func _on_hit():
+	# add destroy animation/particle effects
+	queue_free()
