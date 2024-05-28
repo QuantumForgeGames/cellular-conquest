0d30c7ace375c1aff96196b17e66ae75b4f81591
JoshEthan <joshuaeris@gmail.com>
2024-05-27 20:07:50 -0600

    Fixed scene referencing.

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -4,4 +4,4 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scripts/levels/world.tscn")
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
