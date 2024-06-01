dd8168fbd4475f3913a688890857faf7f9515701
qaptoR <rocco.ruscitti@outlook.com>
2024-06-01 11:07:49 -0700

    audio connections slowly being put into place

--- scripts/entities/cactus_cube/cactus_cube.gd
+++ scripts/entities/cactus_cube/cactus_cube.gd
@@ -12,6 +12,7 @@ extends Organism
 var target: Player = null
 
 func _ready() -> void:
+	Wwise.register_game_obj(self, self.name)
 	$Hitbox.health = initial_health
 	EventManager.game_over.connect(_on_game_over)
 	spawn_spikes()
@@ -25,18 +26,21 @@ func on_absorbed() -> void:
 	tween.tween_callback(queue_free)
 
 func _on_hitbox_damage_recieved(_value: int) -> void:
+	Wwise.post_event_id(AK.EVENTS.PLAY_CACTUS_CUBE_DAMAGE, self)
 	if $Hitbox.health <= 0:
 		on_absorbed()
 
 func set_angry_face() -> void:
 	angry_face.visible = true
 	neutral_face.visible = false
+	Wwise.post_event_id(AK.EVENTS.PLAY_CACTUS_CUBE_VOX, self)
 	
 func set_neutral_face() -> void:
 	angry_face.visible = false
 	neutral_face.visible = true
 
 func launch_spikes() -> void:
+	Wwise.post_event_id(AK.EVENTS.PLAY_CACTUS_CUBE_ATTACK, self)
 	for spike in $Spikes.get_children():
 		spike.launch()
 		
