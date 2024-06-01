6f09aa4e631a388ae03538c5ddd2ecf1fb6ce659
JoshEthan <joshuaeris@gmail.com>
2024-06-01 14:46:22 -0600

    Instructions scene added.

---
+++ scripts/menus/instructions.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_texture_button_pressed():
+	SceneTransition.change_scene("res://scenes/levels/world.tscn")
--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-	SceneTransition.change_scene("res://scenes/levels/world.tscn")
+	SceneTransition.change_scene("res://scenes/menus/instructions.tscn")
 
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
