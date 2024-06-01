ac5b1aa1a6288b22439258a5765cbb6e70743446
qaptoR <rocco.ruscitti@outlook.com>
2024-06-01 07:39:38 -0700

    knockback guy implemented but needs tweaking

---
+++ scripts/entities/knockback_guy/knockback_guy.gd
@@ -0,0 +1,43 @@
+class_name KnockbackGuy
+extends Organism
+
+@export var knockback_cooldown_timer :Timer
+@export var detection_area :Area2D
+@export var knockback_effect :GPUParticles2D
+
+@export var angry_face :Sprite2D
+@export var neutral_face :Sprite2D
+
+@export var CHASE_SPEED :float = 150.0
+@export var DASH_SPEED :float = 1000.0
+@export var DASH_DURATION :float = 0.3
+
+var target: Player = null
+
+func _ready() -> void:
+	$Hitbox.health = initial_health
+	EventManager.game_over.connect(_on_game_over)
+
+func on_absorbed() -> void:
+	$Hitbox.queue_free()
+	EventManager.enemy_died.emit(self)
+	died.emit(self)
+
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(0, 0), 0.5)
+	tween.tween_callback(queue_free)
+	
+func _on_hitbox_damage_received (_value :int) -> void:
+	if $Hitbox.health <= 0:
+		on_absorbed()
+
+func set_angry_face() -> void:
+	angry_face.visible = true
+	neutral_face.visible = false
+
+func set_neutral_face() -> void:
+	angry_face.visible = false
+	neutral_face.visible = true
+
+func _on_game_over():
+	process_mode = Node.PROCESS_MODE_DISABLED
---
+++ scripts/entities/knockback_guy/rage_state.gd
@@ -0,0 +1,41 @@
+extends State
+
+@export var entity: KnockbackGuy
+@export var knockback_particles_scene :PackedScene
+@export var KNOCKBACK_STRENGTH :float = 1000.
+
+var dashing :bool = false
+
+
+func enter () -> void:
+    entity.set_angry_face()
+    _apply_knockback()
+    entity.knockback_cooldown_timer.start()
+
+
+func exit () -> void:
+    entity.knockback_cooldown_timer.stop()
+
+
+func on_detection_area_body_exited (body: Node2D) -> void:
+    if body is Player:
+        entity.target = null
+        transitioned.emit("Idle")
+
+
+func _on_knockback_cooldown_timer_timeout () -> void:
+    _apply_knockback()
+
+
+func _apply_knockback () -> void:
+    var knockback_particles = knockback_particles_scene.instantiate()
+    add_sibling(knockback_particles)
+    knockback_particles.global_position = entity.global_position
+    knockback_particles.scale = entity.scale
+    knockback_particles.emitting = true
+
+    for body in entity.detection_area.get_overlapping_bodies():
+        if body == entity: continue
+        var dir  = body.global_position - entity.global_position
+        # var attenuation = 1 - (dir.length() / entity.detection_area.get_child(0).shape.radius)
+        body.on_knockback(KNOCKBACK_STRENGTH * dir.normalized())
