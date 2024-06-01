c2230f43513cccb7cda49521b4e8ffcb72d31fc6
Akshay <sakshays.2000@gmail.com>
2024-06-01 11:18:00 +0200

    change ui font

--- scripts/entities/knockback.gd
+++ scripts/entities/knockback.gd
@@ -9,10 +9,10 @@ var _radius :float = 0.0
 var _ratio :float = 2.0
 
 func _process (delta :float) -> void:
-    if _radius >= MAX_RADIUS:
-        _ratio = 2.0
-        _radius = 50.0
-        amount_ratio = 1.0
-    amount_ratio = max(0.15, amount_ratio - (delta * _ratio))
-    _ratio += delta * 10
+	if _radius >= MAX_RADIUS:
+		_ratio = 2.0
+		_radius = 50.0
+		amount_ratio = 1.0
+	amount_ratio = max(0.15, amount_ratio - (delta * _ratio))
+	_ratio += delta * 10
 
